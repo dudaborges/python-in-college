642fc3f9b4fad6e3a9cf788270da2aa64bff9345
dudaborges <duda.pborges92@gmail.com>
2023-03-27 17:05:34 -0300

    turtle graphics

---
+++ my-name-turtle.py
@@ -0,0 +1,33 @@
+import turtle as t
+
+# Cria uma instância da tartaruga
+t = t.Turtle()
+
+t.fd(100)
+t.right(150)
+t.fd(60)
+t.left(120)
+t.fd(60)
+t.right(150)
+t.fd(100)
+t.left(90)
+t.fd(20)
+t.left(90)
+t.fd(100)
+t.right(90)
+t.fd(50)
+t.left(180)
+t.fd(50)
+t.left(90)
+t.fd(50)
+t.left(90)
+t.fd(50)
+t.left(180)
+t.fd(50)
+t.left(90)
+t.fd(50)
+t.left(90)
+t.fd(50)
+
+# Mantém a janela aberta até que o usuário a feche
+t.done()
