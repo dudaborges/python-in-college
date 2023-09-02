27813f86400b4ba8a54b4fa3c6cb7e9a2e3a31c7
dudaborges <duda.pborges92@gmail.com>
2023-09-01 21:52:31 -0300

    added exercises

--- exercicios/container.py
+++ exercicios/container.py
@@ -5,20 +5,19 @@ local4 = []
 # 4 locais (pilhas)
 # máximo 3 containers (elementos) cada local (pilha)
 #o container deve ser inserido na pilha (local) com menos elementos. len menos valor
-def empilhar(local):
-    for i in range(3):
-        container = input("Insira o código do container: ")
-        local1.append(container)
-        print(local)
-
 menorTamanho = min(len(local1), len(local2), len(local3), len(local4))
 
-if len(local1) == menorTamanho:
-    empilhar(local1)
-    print(local1)
-elif len(local2) == menorTamanho:
-    print(local2)
-elif len(local3) == menorTamanho:
-    print(local3)
-else:
-    print(local4)
+for i in range(3):
+    container = input("Insira o código do container: ")
+    if len(local1) == menorTamanho:
+        local1.append(container)
+        print(local1)
+    elif len(local2) == menorTamanho:
+        local2.append(container)
+        print(local2)
+    elif len(local3) == menorTamanho:
+        local3.append(container)
+        print(local3)
+    else:
+        local4.append(container)
+        print(local4)
